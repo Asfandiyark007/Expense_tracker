fcaea4e9f9a2332ca8afcee38da6c835ec19399a
Asfandiyark007 <asfandiyar.bsee261@iiu.edu.pk>
2025-04-21 13:53:16 +0500

    modified:   Expense_tracker.py

---
+++ Expense_tracker.py
@@ -0,0 +1,18 @@
+import argparse
+import os
+import json
+
+parser = argparse.ArgumentParser(
+    prog="Expense Tracker",
+    description="Track your expenses and income",
+    epilog="Thank you for using Expense Tracker!",
+)
+
+parser.add_argument("-add", "--count")
+parser.add_argument("-add", "--count")
+parser.add_argument("-add", "--count")
+parser.add_argument("-add", "--count")
+parser.add_argument("-add", "--count")
+parser.add_argument("-add", "--count")
+
+args = parser.parse_args()
